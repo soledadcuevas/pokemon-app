e35b5ca54c9ab17d30d47efa1f096cd5011514df
“soledad <solej444o@gmail.com>
2023-12-03 10:52:58 -0500

    modificacion dle modelo owner

--- owner/models.py
+++ owner/models.py
@@ -7,7 +7,6 @@ class Owner(models.Model):
     dni = models.CharField(max_length=8, default='00000000')
     vigente = models.BooleanField(default=True)
 
-    def __str__(self):
-        return "¨{} de {}".format(self.nombre, self.pais)
+
 
 # Create your models here.
